03f66e8d5d026eae655a85f1f860461647b1c50a
zfusername <farrukh_ziyaev@epam.com>
2023-07-27 16:55:13 +0500

    fix

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.finalTask;
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
         if (divider == 0) {
-            System.out.println("Division by zero");
+            System.out.println("division by zero");
             return;
         }
 
